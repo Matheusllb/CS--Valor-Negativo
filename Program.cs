b804355519944f25f78d78a8737cbbb2b707e1e0
Matheusllb <matheusluislopesbatista@gmail.com>
2023-03-31 15:23:36 -0300

    Descobrindo se valor é negativo

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace CsE7
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Desobrindo valor negativo\n\nINFORME UM VALOR: ");
+            int valor = int.Parse(Console.ReadLine());
+            //SE VALOR É MENOR QUE ZERO É NEGATIVO
+            if (valor < 0)
+                Console.WriteLine("\nNEGATIVO");
+            else
+                Console.WriteLine("\nNAO NEGATIVO!");
+            Console.WriteLine("\nFim do Programa!\n");
+        }
+    }
+}
